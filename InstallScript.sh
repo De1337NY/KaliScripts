777ca506d03bccf8c8b7e52fbbd3f6864825b56d
De1337NY <shepm09@gmail.com>
2022-05-10 11:56:33 -0400

    The Colors Update
    -Added CoLoRs to distinguish script output from command output
    -Fixed confusing wording in output
    -Fixed SecLists and PimpMyKali install paths
    -Now with Better Comments™

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,50 +1,54 @@
 #! /bin/bash
 
+#adding CoLoRs
+red='\033[0;31m'
+green='\033[0;32m'
+purple='\033[0;35m'
+nc='\033[0m' #no color
+
+#start with Kali updates
+echo -e "${green}[+]${purple}Updating Kali...${nc}"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+#checking for and creating transfer directory for webserver hosted files
 if [ ! -d "/home/transfer" ]; then
 	mkdir "/home/transfer"
 fi
 
-if [ ! -d "/opt/SecLists" ]; then
-	mkdir "/opt/SecLists"
-fi
-
-echo "[+] Dropping PimpMyKali in /home/kali..."
+echo -e "${green}[+]${purple}Dropping PimpMyKali in /opt/pimpmykali..${nc}"
 wget https://github.com/Dewalt-arch/pimpmykali/blob/be57a43127a53355454bb90033e6dec4d9075825/pimpmykali.sh -P /opt/
-chmod +x /home/kali/pimpmykali.sh
 
-echo "[+] Installing GoLang..."
+echo -e "${green}[+]${purple}Installing GoLang...${nc}"
 sudo apt install -y golang
 export PATH="$PATH:$HOME/go/bin"
 
-echo "[+] Installing Assetfinder..."
+echo -e "${green}[+]${purple}Installing Assetfinder...${nc}"
 go install -v github.com/tomnomnom/assetfinder@latest
 
-echo "[+] Installing AMASS..."
+echo -e "${green}[+]${purple}Installing AMASS...${nc}"
 go install -v github.com/OWASP/Amass/v3/...@master
 
-echo "[+] Installing HTTProbe..."
+echo -e "${green}[+]${purple}Installing HTTProbe...${nc}"
 go install -v github.com/tomnomnom/httprobe@latest
 
-echo "[+] Installing GoWitness..."
+echo -e "${green}[+]${purple}Installing GoWitness...${nc}"
 go install -v github.com/sensepost/gowitness@latest
 
-echo "[+] Dropping LinPeas in /home/transfer..."
+echo -e "${green}[+]${purple}Dropping LinPeas in /home/transfer...${nc}"
 wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 -P /home/transfer/
 
-echo "[+] Dropping WinPeas in /home/transfer..."
+echo -e "${green}[+]${purple}Dropping WinPeas in /home/transfer...${nc}"
 wget https://github.com/carlospolop/PEASS-ng/blob/585fcc33b20bc68763ed15bcf6b9d49c507aadc2/winPEAS/winPEASbat/winPEAS.bat -P /home/transfer
 
-echo "[+] Dropping Powerview in /home/transfer..."
+echo -e "${green}[+]${purple}Dropping Powerview in /home/transfer...${nc}"
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1 -P /home/transfer
 
-echo "[+] Installing Awesome-Nmap-Grep..."
+echo -e "${green}[+]${purple}Installing Awesome-Nmap-Grep...${nc}"
 sudo git clone https://github.com/leonjza/awesome-nmap-grep.git /opt/awesome-nmap-grep
 
-echo "[+] Dropping SecLists in /opt/SecLists"
+echo -e "${green}[+]${purple}Dropping SecLists in /opt/SecLists${nc}"
 git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
 
-echo "[+] Starting PimpMyKali..."
-./opt/pimpmykali.sh
+echo -e "${green}[+]${purple}Starting PimpMyKali...${nc}"
+./opt//pimpmykali/pimpmykali.sh
